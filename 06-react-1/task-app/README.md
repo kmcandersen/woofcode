e086cb2274fff187d59ddc93a9588d8fc354136c
kmcandersen <kristen.andersen@gmail.com>
2021-03-01 15:01:23 -0600

    week 6 exercises

---
+++ 06-react-1/task-app/README.md
@@ -0,0 +1,5 @@
+# To-do List App
+
+- React with React-Bootstrap
+- Add/remove items
+- Bonus homework for Week 6 of [Woofcode](https://www.woofcode.com/learn/week6/bonus-homework)
---
+++ 06-react-1/task-app/src/App.js
@@ -0,0 +1,75 @@
+// logic - inputs & manage state
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Col, Form, Button, FormControl } from 'react-bootstrap';
+import Header from './components/Header';
+import Overview from './components/Overview';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      task: { id: 0, text: '' },
+      tasks: [],
+      idCounter: 0,
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      task: { id: this.state.idCounter, text: event.target.value },
+    });
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    // form req when state change dep on existing state
+    this.setState((prevState) => ({
+      tasks: [...this.state.tasks, this.state.task],
+      task: { id: 0, text: '' },
+      idCounter: prevState.idCounter + 1,
+    }));
+  };
+  handleRemove = (taskId) => {
+    this.setState({ tasks: this.state.tasks.filter((el) => el.id !== taskId) });
+  };
+
+  render() {
+    const { task, tasks } = this.state;
+    return (
+      <>
+        <Header />
+        <Container className='d-flex justify-content-center'>
+          <Col md={8}>
+            <Form
+              className='mt-4 mb-5'
+              style={{ paddingTop: '56px' }}
+              onSubmit={this.handleSubmit}
+            >
+              <Form.Row>
+                <Col md={10}>
+                  <FormControl
+                    placeholder='New task'
+                    aria-label='New task'
+                    aria-describedby='basic-addon2'
+                    value={task.text}
+                    type='text'
+                    onChange={this.handleChange}
+                  />
+                </Col>
+                <Col md={2}>
+                  <Button variant='warning' type='submit' block>
+                    Add
+                  </Button>
+                </Col>
+              </Form.Row>
+            </Form>
+
+            <Overview tasks={tasks} handleRemove={this.handleRemove} />
+          </Col>
+        </Container>
+      </>
+    );
+  }
+}
+
+export default App;
