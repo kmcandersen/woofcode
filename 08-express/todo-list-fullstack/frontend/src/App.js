1a6de76ac8c792e2df209f380d7f162d336449e4
kmcandersen <kristen.andersen@gmail.com>
2021-03-12 18:06:21 -0600

    remove unnecessary Providers

--- 08-express/todo-list-fullstack/frontend/src/App.js
+++ 08-express/todo-list-fullstack/frontend/src/App.js
@@ -3,19 +3,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Main from './components/Main';
 import { TasksProvider } from './contexts/TasksContext';
-import { TaskProvider } from './contexts/TaskContext';
-import { IdCounterProvider } from './contexts/IdCounterContext';
+// import { TaskProvider } from './contexts/TaskContext';
+// import { IdCounterProvider } from './contexts/IdCounterContext';
 
 const App = () => {
   return (
     <>
       <Header />
       <TasksProvider>
-        <TaskProvider>
-          <IdCounterProvider>
-            <Main />
-          </IdCounterProvider>
-        </TaskProvider>
+        <Main />
       </TasksProvider>
     </>
   );
