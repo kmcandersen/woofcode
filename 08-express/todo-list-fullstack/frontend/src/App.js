502248bb8ad7b88bd436bb4c47f3d6e1fea1528e
kmcandersen <kristen.andersen@gmail.com>
2021-03-13 23:21:41 -0600

    remove unneeded Providers

--- 08-express/todo-list-fullstack/frontend/src/App.js
+++ 08-express/todo-list-fullstack/frontend/src/App.js
@@ -3,8 +3,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Main from './components/Main';
 import { TasksProvider } from './contexts/TasksContext';
-// import { TaskProvider } from './contexts/TaskContext';
-// import { IdCounterProvider } from './contexts/IdCounterContext';
 
 const App = () => {
   return (
